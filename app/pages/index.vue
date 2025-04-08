3c4b47211ee1d78d745598a55be288e554b738f0
Joe Paolicelli <joe@joepaolicelli.com>
2025-04-08 01:52:49 -0700

    feat: intro design for desktop

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,45 +1,176 @@
 <script setup lang="ts">
-const runtimeConfig = useRuntimeConfig()
-const colors = ['#f87171', '#fb923c', '#fbbf24', '#facc15', '#a3e635', '#4ade80', '#34d399', '#2dd4bf', '#22d3ee', '#38bdf8', '#60a5fa', '#818cf8', '#a78bfa', '#c084fc', '#e879f9', '#f472b6', '#fb7185']
-const color = useState('color', () => colors[Math.floor(Math.random() * colors.length)])
+import { motion, useTime, useTransform } from 'motion-v';
+
+const nameTextRef = useTemplateRef('nameTextRef');
+const nameTextBounding = reactive(useElementBounding(nameTextRef));
+
+const picDivTop = ref('0px');
+const picDivLeft = ref('0px');
+const welcomeTextHeight = ref(0);
+
+const gradientDiv = reactive({
+  top: '0px',
+  left: '0px',
+  height: '90vh',
+  width: '750px',
+});
+
+watch(nameTextBounding, (newBounding) => {
+  picDivTop.value = `${newBounding.bottom - 366}px`;
+  picDivLeft.value = `${newBounding.right + 32}px`;
+  welcomeTextHeight.value = newBounding.height;
+
+  gradientDiv.left = `${newBounding.left - 775}px`;
+  console.log(newBounding.left);
+  console.log(gradientDiv.left);
+});
+
+const draw = {
+  hidden: { pathLength: 0, opacity: 0 },
+  visible: (i: unknown) => {
+    const delay = typeof i === 'number' ? i * 0.5 : 0.5;
+    return {
+      pathLength: 1,
+      opacity: 1,
+      transition: {
+        pathLength: { delay, type: 'spring', duration: 1.5, bounce: 0 },
+        opacity: { delay, duration: 0.01 },
+      },
+    };
+  },
+};
+
+const shape = {
+  strokeWidth: 3,
+  strokeLinecap: 'round',
+  fill: 'transparent',
+};
+
+const time = useTime();
+// const orbit = useTransform(time, [0, 4000], [0, 360], { clamp: false });
+const orbitingIcon = {
+  x: useTransform(
+    () => Math.cos(((2 * Math.PI) / 25000) * (time.get() % 25000)) * 300,
+  ),
+  y: useTransform(
+    () => Math.sin(((2 * Math.PI) / 25000) * (time.get() % 25000)) * 300,
+  ),
+};
 </script>
 
 <template>
-  <div class="centered">
-    <h1 :style="{ color }">
-      {{ runtimeConfig.public.helloText }}
-    </h1>
-    <NuxtLink to="/" external>
-      refresh
-    </NuxtLink>
+  <div class="container mx-auto px-4 py-4">
+    <div class="gradientDiv fixed -z-10" :style="gradientDiv"></div>
+    <div
+      class="picDiv fixed w-fit bg-white"
+      flex="~"
+      border="2 rounded-2xl blue-300"
+      shadow="lg blue-300/50"
+      p="4"
+    >
+      <img src="public/me.jpg" class="max-h-[300px] rounded-2xl shadow-xl" />
+    </div>
+    <div class="w-fit md:h-[100vh] m-auto">
+      <div class="flex flex-col justify-center h-full ml-[-100px] my-auto">
+        <motion.svg
+          width="1000"
+          height="1000"
+          viewBox="0 0 1000 1000"
+          initial="hidden"
+          animate="visible"
+          class="absolute ml-[-50px] -z-50"
+        >
+          <motion.circle
+            class="circle-path"
+            cx="0"
+            cy="500"
+            r="300"
+            stroke="oklch(80.9% 0.105 251.813 / 30%)"
+            :variants="draw"
+            :custom="1"
+            :style="shape"
+          />
+        </motion.svg>
+        <motion.div
+          :style="orbitingIcon"
+          text="3xl"
+          class="i-logos-vue absolute ml-[-65px] -z-30"
+        />
+        <div
+          class="w-fit bg-white"
+          flex="~ col"
+          font="kufam semibold"
+          border="2 rounded-2xl blue-300"
+          shadow="lg blue-300/50"
+          p="x-5 t-7 b-4"
+        >
+          <div class="text-8xl/28">Hi.</div>
+          <div ref="nameTextRef" class="text-5xl/16 w-fit pl-1">
+            I'm Joe Paolicelli,
+          </div>
+          <div class="text-5xl/16 pl-1">a Full Stack Developer.</div>
+        </div>
+        <div class="flex gap-5 py-5">
+          <div>
+            <a
+              href="https://github.com/joepaolicelli"
+              target="_blank"
+              aria-label="GitHub"
+              ><button
+                type="button"
+                text="5xl"
+                class="bg-white"
+                border="4 rounded-xl"
+                p="3"
+                cursor="pointer"
+              >
+                <div class="i-logos-github-icon"></div>
+              </button>
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.linkedin.com/in/joepaolicelli/"
+              target="_blank"
+              aria-label="LinkedIn"
+              ><button
+                type="button"
+                text="5xl"
+                class="bg-white"
+                border="4 rounded-xl"
+                p="3"
+                cursor="pointer"
+              >
+                <div class="i-logos-linkedin-icon"></div>
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="grid md:grid-cols-[65%_35%]"></div>
   </div>
 </template>
 
+<style>
+body {
+  background-color: #f9f9f9;
+}
+</style>
+
 <style scoped>
-.centered {
-  position: absolute;
-  width: 100%;
-  text-align: center;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  margin: 0;
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+.picDiv {
+  top: v-bind(picDivTop);
+  left: v-bind(picDivLeft);
 }
-h1 {
-  font-size: 32px;
-}
-@media (min-width: 768px) {
-  h1 {
-    font-size: 64px;
-  }
-}
-a {
-  color: #888;
-  text-decoration: none;
-  font-size: 18px;
-}
-a:hover {
-  text-decoration: underline;
+
+.gradientDiv {
+  background: linear-gradient(
+    to left,
+    transparent,
+    transparent 1%,
+    #f9f9f9 5%,
+    #f9f9f9 0
+  );
 }
 </style>
